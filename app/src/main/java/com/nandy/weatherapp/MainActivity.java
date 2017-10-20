9ca3f33bea1f5cb7cd8470044d11da53f7e20713
Jana Kosar <yana.kosar@gmail.com>
2017-10-21 00:01:23 +0300

    Get last known location

--- app/src/main/java/com/nandy/weatherapp/MainActivity.java
+++ app/src/main/java/com/nandy/weatherapp/MainActivity.java
@@ -1,42 +1,163 @@
 package com.nandy.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.nandy.weatherapp.api.WeatherService;
-import com.nandy.weatherapp.model.Location;
-import com.nandy.weatherapp.model.Weather;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
-import java.util.List;
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
-import io.reactivex.SingleObserver;
-import io.reactivex.disposables.Disposable;
 
-public class MainActivity extends AppCompatActivity {
+    private final static int REQUEST_CHECK_SETTINGS = 2000;
+
+    private Location lastKnownLocation;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new WeatherService().search("Lviv")
-                .subscribe(new SingleObserver<List<Location>>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                        Log.d("WEATHER_", "onSubscribe");
-                    }
+        // check availability of play services
+        if (checkPlayServices()) {
 
-                    @Override
-                    public void onSuccess(List<Location> locations) {
-                        Log.d("WEATHER_", "onSuccess: " + locations.size());
-                    }
+            // Building the GoogleApi client
+            buildGoogleApiClient();
+        }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.d("WEATHER_", "onError: " + e);
 
-                    }
-                });
+//        new WeatherService().search("Lviv")
+//                .subscribe(new SingleObserver<List<Location>>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//                        Log.d("WEATHER_", "onSubscribe");
+//                    }
+//
+//                    @Override
+//                    public void onSuccess(List<Location> locations) {
+//                        Log.d("WEATHER_", "onSuccess: " + locations.size());
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        Log.d("WEATHER_", "onError: " + e);
+//
+//                    }
+//                });
     }
+
+    private void getLocation() {
+
+
+        try {
+            lastKnownLocation = LocationServices.FusedLocationApi
+                    .getLastLocation(googleApiClient);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+    protected synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+
+        googleApiClient.connect();
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+
+        result.setResultCallback(locationSettingsResult -> {
+
+            final Status status = locationSettingsResult.getStatus();
+
+            switch (status.getStatusCode()) {
+                case LocationSettingsStatusCodes.SUCCESS:
+                    getLocation();
+                    break;
+                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                    try {
+                        status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        });
+
+
+    }
+
+
+    private boolean checkPlayServices() {
+
+
+        return  GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        getLocation();
+                        break;
+                }
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i("LOCATION_", "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        getLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        googleApiClient.connect();
+    }
+
+
 }
